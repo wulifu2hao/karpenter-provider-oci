42128842405bd7e5fdf1c0f81b254abb8797daa9
Lifu <wulifu20hao@gmail.com>
2026-09-09 11:08:11 +0800

    refactor: move the capacity controller to pkg/controllers/instancetype/capacity
    It sat under pkg/controllers/providers/..., copied from the AWS provider's
    layout. That path reads as though the package were a provider implementation,
    when it is a controller that consumes one. It also did not match this repo's
    own convention, where controllers sit directly under pkg/controllers.

--- pkg/controllers/controllers.go
+++ pkg/controllers/controllers.go
@@ -13,7 +13,7 @@ import (
 	"github.com/awslabs/operatorpkg/controller"
 	"github.com/oracle/karpenter-provider-oci/pkg/controllers/nodeclasses"
 	"github.com/oracle/karpenter-provider-oci/pkg/controllers/orphaninstance"
-	"github.com/oracle/karpenter-provider-oci/pkg/controllers/providers/instancetype/capacity"
+	"github.com/oracle/karpenter-provider-oci/pkg/controllers/instancetype/capacity"
 	"github.com/oracle/karpenter-provider-oci/pkg/providers/capacityreservation"
 	"github.com/oracle/karpenter-provider-oci/pkg/providers/clusterplacementgroup"
 	"github.com/oracle/karpenter-provider-oci/pkg/providers/computecluster"
